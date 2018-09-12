b1f040bda162519c58e6da9873a3e97cb536a054
Baoquan Dinh <baoquanxdinh@gmail.com>
2018-09-11 18:07:00 -0700

    Home Page: Added wrappers in the list creation to separate image wrapper from the information wrapper. Made cosmetic changes to the UI to make it more mobile friendly.

--- js/main.js
+++ js/main.js
@@ -137,28 +137,34 @@ fillRestaurantsHTML = (restaurants = self.restaurants) => {
  */
 createRestaurantHTML = (restaurant) => {
   const li = document.createElement('li');
+  const imageDiv = document.createElement('div');
+  const infoDiv = document.createElement('div');
 
   const image = document.createElement('img');
   image.className = 'restaurant-img';
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
-  li.append(image);
+  imageDiv.append(image);
+  li.append(imageDiv);
+  
 
   const name = document.createElement('h1');
   name.innerHTML = restaurant.name;
-  li.append(name);
+  infoDiv.append(name);
 
   const neighborhood = document.createElement('p');
   neighborhood.innerHTML = restaurant.neighborhood;
-  li.append(neighborhood);
+  infoDiv.append(neighborhood);
 
   const address = document.createElement('p');
   address.innerHTML = restaurant.address;
-  li.append(address);
+  infoDiv.append(address);
 
   const more = document.createElement('a');
   more.innerHTML = 'View Details';
   more.href = DBHelper.urlForRestaurant(restaurant);
-  li.append(more)
+  infoDiv.append(more)
+
+  li.append(infoDiv);
 
   return li
 }
