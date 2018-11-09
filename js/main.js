11347cb7985d8513bdcbaebc0bc38a4329404df6
Baoquan Dinh <baoquanxdinh@gmail.com>
2018-11-09 09:36:46 -0800

    Downloaded original repository from github.

--- js/main.js
+++ js/main.js
@@ -136,44 +136,29 @@ fillRestaurantsHTML = (restaurants = self.restaurants) => {
  * Create restaurant HTML.
  */
 createRestaurantHTML = (restaurant) => {
-
   const li = document.createElement('li');
-  const imageDiv = document.createElement('div');
-  const infoDiv = document.createElement('div');
-  imageDiv.className = "resturant-img-div";
-  infoDiv.className = "resturant-info-div";
-
-  if (document.querySelectorAll("li").length === 0) {
-    li.id = "restaurants-list-first"
-  }
 
   const image = document.createElement('img');
   image.className = 'restaurant-img';
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
-  imageDiv.append(image);
-  li.append(imageDiv);
-  
+  li.append(image);
 
   const name = document.createElement('h1');
   name.innerHTML = restaurant.name;
-  infoDiv.append(name);
+  li.append(name);
 
   const neighborhood = document.createElement('p');
   neighborhood.innerHTML = restaurant.neighborhood;
-  infoDiv.append(neighborhood);
+  li.append(neighborhood);
 
   const address = document.createElement('p');
   address.innerHTML = restaurant.address;
-  infoDiv.append(address);
+  li.append(address);
 
   const more = document.createElement('a');
   more.innerHTML = 'View Details';
   more.href = DBHelper.urlForRestaurant(restaurant);
-  infoDiv.append(more)
-
-  li.append(infoDiv);
-
-  
+  li.append(more)
 
   return li
 }
